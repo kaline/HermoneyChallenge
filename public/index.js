9f7b50726a27ee38ed1b8246450762bee8d2175f
kaline <mesquitabfkaline@gmail.com>
2020-08-06 01:59:19 -0300

    :sparkles:

--- public/index.js
+++ public/index.js
@@ -1,11 +1,79 @@
+var array = [];
 
-function requestMusicData(){
+(function(angular) {
 
-const http = new XMLHttpRequest()
+  'use strict';
 
-http.open("GET","http://localhost:3000/music")
-http.send()
+angular.module('httpExample', [])
 
-http.onload = () => console.log(http.responseText)
+  .config(['$sceDelegateProvider', function($sceDelegateProvider) {
 
-}
+   
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+
+      'self',
+
+      'https://angularjs.org/**'
+
+    ]);
+
+  }])
+
+  .controller('FetchController', ['$scope', '$http', '$templateCache',
+
+    function($scope, $http, $templateCache) {
+
+      $scope.method = 'GET';
+
+      $scope.url = 'http://localhost:3000/music';
+
+      $scope.fetch = function() {
+
+        $scope.code = null;
+
+        $scope.response = null;
+
+
+
+        $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+	 
+	  
+          then(function(response) {
+
+            $scope.status = response.status;
+
+            $scope.data = response.data;
+
+		for(var i = 0; i < $scope.data.length; i++){
+			//array[i].push($scope.data[i].array[i]);
+	                //console.log(array[i]);	
+		}
+
+          }, function(response) {
+
+            $scope.data = response.data || 'Request failed';
+
+            $scope.status = response.status;
+
+        });
+
+      };
+
+
+
+      $scope.updateModel = function(method, url) {
+
+        $scope.method = method;
+
+        $scope.url = url;
+
+      };
+
+    }]);
+
+})(window.angular);
+
+
+
+
