dd1d2d0c94a4959e7f2856ecd51d2a351f9cdc15
kaline <mesquitabfkaline@gmail.com>
2020-08-06 20:02:23 -0300

    add package.json

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 //const request = require('request');
+const path = require('path') 
+
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public'))) 
+
+
+app.get('/', function(req, res){ 
+    res.render('Demo') 
+}) 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
